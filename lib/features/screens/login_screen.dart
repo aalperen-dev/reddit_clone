c8a61535e3d06ae2032e36c69965117cb5131db6
Alperen Alperen <aalperen@icloud.com>
2024-01-20 18:40:27 +0300

    login progress

--- lib/features/screens/login_screen.dart
+++ lib/features/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../core/constants/constants.dart';
+
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
@@ -7,9 +9,38 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Title'),
+        centerTitle: true,
+        title: Image.asset(
+          Assets.assetsImagesLogo,
+          height: 40,
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: const Text(
+              'Skip',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          const SizedBox(height: 30),
+          const Text(
+            'Dive into anything',
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 0.5),
+          ),
+          const SizedBox(height: 30),
+          Image.asset(
+            Assets.assetsImagesLoginEmote,
+            height: 400,
+          ),
+        ],
+      ),
     );
   }
 }
