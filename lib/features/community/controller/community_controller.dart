32f44268af16855ba855a8b3f58fb4d8d8f403bf
Alperen Alperen <aalperen@icloud.com>
2024-01-31 18:20:06 +0300

    progress

--- lib/features/community/controller/community_controller.dart
+++ lib/features/community/controller/community_controller.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/features/community/repository/community_repository.dart';
+import 'package:reddit_clone/features/controller/auth_controller.dart';
+import 'package:reddit_clone/models/community_model.dart';
+import 'package:routemaster/routemaster.dart';
 
+import '../../../core/utils/utilities.dart';
+
+class CommunityController extends StateNotifier<bool> {
+  final CommunityRepository _communityRepository;
+  final Ref _ref;
+
+  CommunityController(
+      {required CommunityRepository communityRepository, required Ref ref})
+      : _communityRepository = communityRepository,
+        _ref = ref,
+        super(false);
+
+  void createCommunity(String name, BuildContext context) async {
+    state = true;
+    final uid = _ref.read(userProvider)?.uid ?? '';
+
+    CommunityModel communityModel = CommunityModel(
+      id: name,
+      name: name,
+      banner: Assets.bannerDefault,
+      avatar: Assets.avatarDefault,
+      members: [uid],
+      mods: [uid],
+    );
+
+    final res = await _communityRepository.createCommunity(communityModel);
+    state = false;
+    res.fold((l) => showSnackBar(context, l.message), (r) {
+      showSnackBar(context, 'Community created successfully!');
+      Routemaster.of(context).pop();
+    });
+  }
+}
