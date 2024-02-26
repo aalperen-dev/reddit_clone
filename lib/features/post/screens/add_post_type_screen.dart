7edd08f0263824ed9df6257381af81cce80d33c2
Alperen Alperen <aalperen@icloud.com>
2024-02-26 17:07:12 +0300

    progress

--- lib/features/post/screens/add_post_type_screen.dart
+++ lib/features/post/screens/add_post_type_screen.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reddit_clone/core/common/error_text.dart';
+import 'package:reddit_clone/core/common/loader.dart';
+import 'package:reddit_clone/features/community/controller/community_controller.dart';
+import 'package:reddit_clone/models/community_model.dart';
+import '../../../core/utils/utilities.dart';
+import '../../../theme/pallete.dart';
 
 class AddPostTypeScreen extends ConsumerStatefulWidget {
   final String type;
@@ -14,14 +23,32 @@ class AddPostTypeScreen extends ConsumerStatefulWidget {
 
 class _AddPostTypeScreenState extends ConsumerState<AddPostTypeScreen> {
   final titleController = TextEditingController();
+  final descController = TextEditingController();
+  final linkController = TextEditingController();
+  File? bannerFile;
+  List<CommunityModel> communities = [];
+  CommunityModel? selectedCommunity;
   @override
   void dispose() {
     super.dispose();
     titleController.dispose();
+    descController.dispose();
+    linkController.dispose();
+  }
+
+  void selectBannerImage() async {
+    final res = await pickImage();
+
+    if (res != null) {
+      setState(() {
+        bannerFile = File(res.files.first.path!);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final currentTheme = ref.watch(themeNotifierProvider);
     final isTypeImage = widget.type == 'image';
     final isTypeText = widget.type == 'text';
     final isTypeLink = widget.type == 'link';
@@ -48,8 +75,93 @@ class _AddPostTypeScreenState extends ConsumerState<AddPostTypeScreen> {
                 contentPadding: EdgeInsets.all(18),
               ),
               controller: titleController,
+              maxLength: 30,
             ),
             const SizedBox(height: 10),
+            //
+            if (isTypeImage)
+              GestureDetector(
+                onTap: selectBannerImage,
+                child: DottedBorder(
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(15),
+                  dashPattern: const [10, 4],
+                  strokeCap: StrokeCap.round,
+                  color: currentTheme.textTheme.bodyMedium!.color!,
+                  child: Container(
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: bannerFile != null
+                        ? Image.file(bannerFile!)
+                        : const Center(
+                            child: Icon(
+                              Icons.camera_alt_outlined,
+                              size: 40,
+                            ),
+                          ),
+                  ),
+                ),
+              ),
+            //
+            if (isTypeText)
+              TextField(
+                decoration: const InputDecoration(
+                  filled: true,
+                  hintText: 'Enter description here',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(18),
+                ),
+                controller: descController,
+                maxLines: 5,
+              ),
+            //
+            if (isTypeLink)
+              TextField(
+                decoration: const InputDecoration(
+                  filled: true,
+                  hintText: 'Enter link here',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.all(18),
+                ),
+                controller: linkController,
+              ),
+            const SizedBox(height: 20),
+            const Align(
+              alignment: Alignment.topLeft,
+              child: Text('Select Community'),
+            ),
+            //
+            ref.watch(userCommunitiesProvider).when(
+                  data: (data) {
+                    communities = data;
+                    if (data.isEmpty) {
+                      return const SizedBox.shrink();
+                    }
+                    return DropdownButton(
+                      value: selectedCommunity ?? data[0],
+                      items: data
+                          .map(
+                            (e) => DropdownMenuItem(
+                              value: e,
+                              child: Text(e.name),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          selectedCommunity = value;
+                        });
+                      },
+                    );
+                  },
+                  error: (error, stackTrace) => ErrorText(
+                    error: error.toString(),
+                  ),
+                  loading: () => const Loader(),
+                ),
           ],
         ),
       ),
